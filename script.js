bf2f84f49ed4363e672eda44487b7a8d4064f2ed
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-16 15:53:40 +0000

    more errors

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 document.getElementById('mybtn').addEventListener('click', Seeifyourright)
 
-function  Seeifyourright() {
-  const password = parseInt(Math.random()* 10)
-  const input = document.getElementById('input').value
-  if (password == input) {
+function Seeifyourright() {
+  const password = parseInt(Math.random() *10)
+  const input = parseInt(document.getElementById('input').value);
+  if (password === input) {
     alert('correct')
   }
   else {
-    alert('Incorrect'+ password)
+    alert('Incorrect' +password)
   }
 }
