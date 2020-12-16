f13c8c092fd616046dd1e97942683a223421d8c5
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-16 15:13:43 +0000

    button and texbox

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+document.getElementById('mybtn').addEventListener('click', Seeifyourright)
+
+function Seeifyourright() {
+  let password= "Math.random() *10"
+  let input = document.getElementById('input').value
+  if (password == input) {
+    alert ("correct");
+  }
+}
