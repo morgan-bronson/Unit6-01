7942e29e131cf312d7c06a8c34fafcfe242a0162
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-16 15:38:44 +0000

    shows me correct answer when i get it wrong

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 document.getElementById('mybtn').addEventListener('click', Seeifyourright)
 
 function Seeifyourright() {
-  let password= "Math.random() *10"
+  let password= parseInt (Math.random() *10)
   let input = document.getElementById('input').value
   if (password == input) {
     alert ("correct");
   }
   else {
-    alert ("Incorrect")
+    alert ("Incorrect"+password);
   }
 }
